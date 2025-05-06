860d97f6d9518c082529743682f3c3accc7cdec3
strforexcel <117307203+strforexcel@users.noreply.github.com>
2025-05-06 10:44:16 +0700

    Edit API endpoint

--- frontend/src/Util/apiHandler.js
+++ frontend/src/Util/apiHandler.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api"; // replace with your actual backend URL
+const API_BASE_URL = import.meta.env.API_BASE_URL; // replace with your actual backend URL
 
 const api = axios.create({
   baseURL: API_BASE_URL,
