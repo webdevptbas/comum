0f5388b6cf2c92aae802b034c47bb1ff8a544e85
strforexcel <117307203+strforexcel@users.noreply.github.com>
2025-05-14 14:31:13 +0700

    Add back button on Article list page

--- frontend/src/Pages/ArticleListPage/ArticleListPage.jsx
+++ frontend/src/Pages/ArticleListPage/ArticleListPage.jsx
@@ -2,10 +2,21 @@
 import React from "react";
 import "./ArticleListPage.css"; // Create this new CSS file for standalone layout
 import ArticleList from "../../Component/CommunityComponent/ArticleList/ArticleList";
+import { useNavigate } from "react-router";
+import { ArrowLeftOutlined } from "@ant-design/icons";
 
 const ArticleListPage = () => {
+  const navigate = useNavigate();
+  
   return (
     <div className="articlelist-page-container">
+      <div
+        className="past-event-back-button"
+        onClick={() => navigate("/community")}
+      >
+        <ArrowLeftOutlined />
+        <div>Back</div>
+      </div>
       <h1 className="title articlelist-page-title">Articles</h1>
       <div className="articlelist-page-content">
         <ArticleList />
