dc0dd439d87d676aa4f9db965845620e2a18ebda
Web Developer BAS <webdeveloper.bas@gmail.com>
2026-03-13 09:59:43 +0700

    Revise cart slice

--- frontend/src/Slices/cartSlice.js
+++ frontend/src/Slices/cartSlice.js
@@ -1,8 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../Util/CartUtils";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
-  : { cartItems: [] };
+  : {
+      cartItems: [],
+      itemsPrice: 0,
+      shippingPrice: 0,
+      totalPrice: 0,
+    };
 
 const addDecimals = (num) => {
   return (Math.round(num * 100) / 100).toFixed(2);
@@ -18,34 +24,19 @@ const cartSlice = createSlice({
       const existItem = state.cartItems.find((x) => x._id === item._id);
 
       if (existItem) {
+        const updatedQty = Math.min(
+          existItem.quantity + item.quantity,
+          item.stock,
+        );
+
         state.cartItems = state.cartItems.map((x) =>
-          x._id === existItem._id ? item : x,
+          x._id === existItem._id ? { ...x, quantity: updatedQty } : x,
         );
       } else {
-        state.cartItems = [...state.cartItems];
+        state.cartItems = [...state.cartItems, item];
       }
 
-      //calculate items price
-      state.itemsPrice = addDecimals(
-        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0),
-      );
-
-      //calculate shipping price (if order over 100000 then free, else 10000 shipping)
-      state.shippingPrice = addDecimals(state.itemsPrice > 100000 ? 0 : 10000);
-
-      //calculate tax price (15% tax)
-      state.taxPrice = addDecimals(
-        Number((0.15 * state.itemsPrice).toFixed(2)),
-      );
-
-      //calculate total price
-      state.totalPrice = (
-        Number(state.itemsPrice) +
-        Number(state.shippingPrice) +
-        Number(state.taxPrice)
-      ).toFixed(2);
-
-      localStorage.setItem("cart", JSON.stringify(state));
+      return updateCart(state);
     },
   },
 });
