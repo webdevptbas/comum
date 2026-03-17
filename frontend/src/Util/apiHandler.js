3426b4cc485d2f75af8d999fd87562976c563ff9
strforexcel <117307203+strforexcel@users.noreply.github.com>
2026-03-18 00:20:02 +0700

    Minor revision

--- frontend/src/Util/apiHandler.js
+++ frontend/src/Util/apiHandler.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
-// const API_BASE_URL = "http://192.168.18.5:5000/api";
+// const API_BASE_URL = "http://172.30.53.14:5000/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
