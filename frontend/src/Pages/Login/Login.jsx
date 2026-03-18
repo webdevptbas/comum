99e2b1aa2aad71972c88a68d8f7e7d6c257e0206
Web Developer BAS <webdeveloper.bas@gmail.com>
2026-03-18 16:27:44 +0700

    Minor revision

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Form, Input, Button, message } from "antd";
 import { useNavigate, useLocation } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
