cbab8e6a1c731a4eb099cd869ec366f92a1b964b
Web Developer BAS <webdeveloper.bas@gmail.com>
2026-03-19 15:19:56 +0700

    Create new ProfileLayout component

---
+++ frontend/src/Layout/ProfileLayout/ProfileLayout.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { Outlet, useNavigate, useLocation } from "react-router";
+import { useDispatch } from "react-redux";
+
+import "./ProfileLayout.css";
+import { clearCredentials } from "../../Slices/authSlice";
+import { useLogoutMutation } from "../../Slices/usersApiSlice";
+import { FaSignOutAlt } from "react-icons/fa";
+import { menu } from "./item";
+
+const ProfileLayout = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const dispatch = useDispatch();
+
+  const [pageTitle, setPageTitle] = useState(menu[0].label);
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const handleLogout = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(clearCredentials());
+      navigate("/login");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <>
+      <div className="profile-container">
+        <h2>{pageTitle}</h2>
+        <div className="profile-page-content">
+          {/* Sidebar */}
+          <div className="profile-sidebar">
+            {menu.map((item) => (
+              <div
+                key={item.path}
+                className={`profile-menu-item ${
+                  location.pathname === item.path ? "active" : ""
+                }`}
+                onClick={() => {
+                  navigate(item.path);
+                  setPageTitle(item.label);
+                }}
+              >
+                {item.icon}
+                <span>{item.label}</span>
+              </div>
+            ))}
+
+            <div className="profile-menu-item logout" onClick={handleLogout}>
+              <FaSignOutAlt />
+              <span>Logout</span>
+            </div>
+          </div>
+
+          {/* Content */}
+          <div className="profile-content">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProfileLayout;
