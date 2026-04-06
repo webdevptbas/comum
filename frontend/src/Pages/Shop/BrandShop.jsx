27b4676383acc4a1be0f65f7e97b9e5c0c975497
Web Developer BAS <webdeveloper.bas@gmail.com>
2026-04-06 11:05:16 +0700

    Add new Brand Shop page

---
+++ frontend/src/Pages/Shop/BrandShop.jsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useState, useMemo } from "react";
+import { Link, useLocation, useParams } from "react-router";
+import { ProductCard } from "../../Component/Card/Card";
+import { useGetProductsQuery } from "../../Slices/productsApiSlice";
+import { fetchBrands } from "../../Util/apiService";
+
+const BrandShopPage = () => {
+  const { brand } = useParams();
+  const { search } = useLocation();
+
+  const [brands, setBrands] = useState([]);
+  const [loadingBrands, setLoadingBrands] = useState(true);
+
+  const params = new URLSearchParams(search);
+
+  const gender = params.get("gender");
+  const category = params.get("category");
+
+  const brandId = useMemo(() => {
+    if (!brand || brands.length === 0) return null;
+
+    const found = brands.find(
+      (b) => b.name.toLowerCase() === brand.toLowerCase(),
+    );
+
+    return found?._id || null;
+  }, [brand, brands]);
+
+  const shouldFetchProducts = !loadingBrands && brandId;
+
+  const queryParams = {
+    ...(brandId && { brand: brandId }),
+    ...(gender && { gender }),
+    ...(category && { category }),
+  };
+
+  useEffect(() => {
+    const loadBrands = async () => {
+      try {
+        const data = await fetchBrands();
+        setBrands(data);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoadingBrands(false);
+      }
+    };
+
+    loadBrands();
+  }, []);
+
+  const {
+    data: products,
+    isLoading,
+    error,
+  } = useGetProductsQuery(queryParams, {
+    skip: !shouldFetchProducts,
+  });
+
+  return (
+    <>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <div>{error?.data?.message || error.error}</div>
+      ) : (
+        <>
+          <div className="store-header">
+            <p>{products?.length} Product(s)</p>
+          </div>
+
+          {products?.length === 0 && (
+            <div className="product-grid heading4">No product available</div>
+          )}
+
+          <div className="product-grid">
+            {products?.map((product) => (
+              <Link key={product._id} to={`/shop/${brand}/${product._id}`}>
+                <ProductCard
+                  key={product._id}
+                  title={product.productName}
+                  brand={product.brand?.name}
+                  src={product.imageUrl?.[0]}
+                  price={product.displayPrice}
+                  finalPrice={product.displayDiscountPrice}
+                  type={product.displayIsDiscount ? "discount" : ""}
+                  text={
+                    product.displayIsDiscount
+                      ? `${product.displayDiscount}%`
+                      : ""
+                  }
+                  isDiscount={product.displayIsDiscount}
+                  displayStock={product.displayStock}
+                />
+              </Link>
+            ))}
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default BrandShopPage;
