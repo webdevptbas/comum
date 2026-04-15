04085c48a9cec729206f90094a2b5cd8f9ade2af
Web Developer BAS <webdeveloper.bas@gmail.com>
2026-04-15 18:01:19 +0700

    Minor revision

--- frontend/src/Component/Header/Header.jsx
+++ frontend/src/Component/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Header.css";
 import "../../index.css";
 import useMediaQuery from "../../Util/useMediaQuery";
-import MobileHeader from "./MobileHeader";
+import MobileHeader from "./Mobile/MobileHeader";
 import DesktopHeader from "./Desktop/DesktopHeader";
 
 const MainHeader = () => {
