e1618504940e3e6560117be671f7b61208402e91
Web Developer BAS <webdeveloper.bas@gmail.com>
2026-04-23 17:41:39 +0700

    Add new routes for raja ongkir

---
+++ backend/routes/rajaOngkirRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getProvinces,
+  getCitiesByProvinceId,
+  getDistrictsByCityId,
+  getSubDistrictsByDistrictId,
+} = require("../controllers/RajaOngkirController");
+
+//Public routes
+router.get("/province", getProvinces);
+router.get("/city/:id", getCitiesByProvinceId);
+router.get("/district/:id", getDistrictsByCityId);
+router.get("/sub-district/:id", getSubDistrictsByDistrictId);
+
+module.exports = router;
