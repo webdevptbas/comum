b3ad5e1100206fc839ac892d0dd51678df8cb28b
Web Developer BAS <webdeveloper.bas@gmail.com>
2026-04-24 18:16:10 +0700

    Modify component

--- frontend/src/Component/CheckoutComponent/ShippingMethod.jsx
+++ frontend/src/Component/CheckoutComponent/ShippingMethod.jsx
@@ -1,58 +1,38 @@
 import { Card, Radio } from "antd";
+import { formatRupiah } from "../../Util/CartUtils";
 
-const shippingOptions = [
-  {
-    service: "REG",
-    description: "Layanan Reguler",
-    etd: "2-3 hari",
-    cost: 9000,
-  },
-  {
-    service: "YES",
-    description: "Yakin Esok Sampai",
-    etd: "1 hari",
-    cost: 20000,
-  },
-  {
-    service: "OKE",
-    description: "Ongkos Kirim Ekonomis",
-    etd: "3-4 hari",
-    cost: 8000,
-  },
-];
-
-const ShippingMethod = ({ value, onChange }) => {
+const ShippingMethod = ({ options = [], value, onChange }) => {
+  console.log({ options });
   return (
-    <Card className="checkout-card">
-      <h5 className="heading5 checkout-shipping-title">
-        Shipping Method (JNE)
-      </h5>
+    <Card className="checkout-card shipping-checkout-card">
+      <div className="shipping-inner">
+        <h5 className="heading5 checkout-shipping-title">Shipping Method</h5>
 
-      <Radio.Group
-        className="shipping-radio-group"
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-      >
-        {shippingOptions.map((item) => (
-          <Radio
-            key={item.service}
-            value={item}
-            className="shipping-radio-item"
+        <div className="shipping-scroll-container">
+          <Radio.Group
+            className="shipping-radio-group"
+            value={value}
+            onChange={(e) => onChange(e?.target?.value)}
           >
-            <div className="shipping-content">
-              <div className="shipping-left">
-                <strong>JNE {item.service}</strong>
-                <p className="shipping-desc">{item.description}</p>
-              </div>
-
-              <div className="shipping-right">
-                <strong>Rp{item.cost.toLocaleString("id-ID")}</strong>
-                <p className="shipping-etd">{item.etd}</p>
-              </div>
-            </div>
-          </Radio>
-        ))}
-      </Radio.Group>
+            {options.map((item) => (
+              <Radio
+                key={item?.service}
+                value={item}
+                className="shipping-radio-item"
+              >
+                <div className="shipping-content">
+                  <div className="shipping-left">
+                    <p className="text-l-medium">
+                      {item?.code?.toUpperCase()} {item?.service}
+                    </p>
+                    <p className="text-l-regular">{formatRupiah(item?.cost)}</p>
+                  </div>
+                </div>
+              </Radio>
+            ))}
+          </Radio.Group>
+        </div>
+      </div>
     </Card>
   );
 };
