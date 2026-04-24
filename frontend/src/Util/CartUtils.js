6be099b5687302a7cfbbbf3dddc26c4ffd6c68e3
Web Developer BAS <webdeveloper.bas@gmail.com>
2026-04-24 18:12:31 +0700

    Modify shipping price

--- frontend/src/Util/CartUtils.js
+++ frontend/src/Util/CartUtils.js
@@ -10,8 +10,8 @@ export const updateCart = (state) => {
     state.cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0),
   );
 
-  //calculate shipping price (if order over 100000 then free, else 10000 shipping)
-  state.shippingPrice = addDecimals(state.itemsPrice > 100000 ? 0 : 10000);
+  //calculate shipping price
+  state.shippingPrice = addDecimals(state.shippingPrice || 0);
 
   //calculate total price
   state.totalPrice = (
