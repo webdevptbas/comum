a972282d37d5de178faacc9daad3536cb5ff29e2
Web Developer BAS <webdeveloper.bas@gmail.com>
2026-04-30 17:54:00 +0700

    Move function from Address Section component to Parent

--- frontend/src/Pages/Checkout/Checkout.jsx
+++ frontend/src/Pages/Checkout/Checkout.jsx
@@ -3,24 +3,61 @@ import ShippingMethod from "../../Component/CheckoutComponent/ShippingMethod";
 import CartOverview from "../../Component/CheckoutComponent/CartOverview";
 import SummaryCard from "../../Component/CheckoutComponent/SummaryCard";
 import "./Checkout.css";
-import { Steps } from "antd";
+import { message, Steps } from "antd";
 import { useEffect, useState } from "react";
-import { setShippingPrice } from "../../Slices/cartSlice";
-import { useDispatch } from "react-redux";
+import { saveShippingMethod, setShippingPrice } from "../../Slices/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
 import useMediaQuery from "../../Util/useMediaQuery";
+import { districtCalculateCost } from "../../Util/apiService";
 
 const CheckoutPage = () => {
+  const dispatch = useDispatch();
+  const { shippingAddress, totalWeight, shippingMethod } = useSelector(
+    (state) => state.cart,
+  );
+
   const [shippingOptions, setShippingOptions] = useState([]);
-  const [selectedShipping, setSelectedShipping] = useState(null);
+  const [selectedShipping, setSelectedShipping] = useState(
+    shippingMethod || null,
+  );
   const isMobile = useMediaQuery("(max-width: 1024px)");
 
-  const dispatch = useDispatch();
+  useEffect(() => {
+    if (shippingMethod) {
+      setSelectedShipping(shippingMethod);
+    }
+  }, [shippingMethod]);
 
   useEffect(() => {
     if (selectedShipping) {
       dispatch(setShippingPrice(selectedShipping.cost));
     }
   }, [selectedShipping]);
+
+  useEffect(() => {
+    const fetchShipping = async () => {
+      try {
+        if (!shippingAddress?.district?.value || !totalWeight) return;
+
+        const payload = {
+          destination: shippingAddress.district.value,
+          weight: totalWeight,
+        };
+
+        const res = await districtCalculateCost(payload);
+        setShippingOptions(res?.data || []);
+      } catch (err) {
+        message.error(err);
+      }
+    };
+
+    fetchShipping();
+  }, [shippingAddress, totalWeight]);
+
+  const handleShippingMethod = async (value) => {
+    setSelectedShipping(value);
+    dispatch(saveShippingMethod(value));
+  };
 
   return (
     <div className="checkout-page-container">
@@ -44,9 +81,9 @@ const CheckoutPage = () => {
       <div className="checkout-container">
         {/* LEFT */}
         <div className="checkout-left">
-          <AddressSection setShippingOptions={setShippingOptions} />
+          <AddressSection />
           <ShippingMethod
-            onChange={setSelectedShipping}
+            onChange={handleShippingMethod}
             options={shippingOptions}
             value={selectedShipping}
           />
