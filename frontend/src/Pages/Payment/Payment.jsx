e2b13314156c398331229a5b30f9a72447788303
Web Developer BAS <webdeveloper.bas@gmail.com>
2026-05-07 15:56:22 +0700

    Minor revision on Payment page

--- frontend/src/Pages/Payment/Payment.jsx
+++ frontend/src/Pages/Payment/Payment.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 const PaymentPage = () => {
+  const [paymentMethod, setPaymentMethod] = useState("");
   return (
     <>
       <div>This is the Payment Page</div>
