8a979714bf83d53f2aa258606269e9fdcfb13233
Web Developer BAS <webdeveloper.bas@gmail.com>
2026-05-12 18:20:18 +0700

    Modify Payment page

--- frontend/src/Pages/Payment/Payment.jsx
+++ frontend/src/Pages/Payment/Payment.jsx
@@ -1,7 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router";
 
 const PaymentPage = () => {
   const [paymentMethod, setPaymentMethod] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { shippingAddress } = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    if (!shippingAddress) {
+      navigate("/shipping");
+    }
+  }, [shippingAddress, navigate]);
+
   return (
     <>
       <div>This is the Payment Page</div>
