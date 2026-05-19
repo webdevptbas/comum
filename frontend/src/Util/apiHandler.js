661ee5c382738aece39f051ef6277d3cc1ea82bd
Web Developer BAS <webdeveloper.bas@gmail.com>
2026-05-19 18:06:19 +0700

    Remove unused comments

--- frontend/src/Util/apiHandler.js
+++ frontend/src/Util/apiHandler.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
-// const API_BASE_URL = "http://172.30.53.14:5000/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
