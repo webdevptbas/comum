18a3dade5e49f26655281550b874c83df7926abc
Web Developer BAS <webdeveloper.bas@gmail.com>
2026-05-21 17:50:22 +0700

    Add new variable on .env for Midtrans config

--- backend/config/midtrans.js
+++ backend/config/midtrans.js
@@ -2,14 +2,14 @@ const midtransClient = require("midtrans-client");
 
 //core API instance
 let coreApi = new midtransClient.CoreApi({
-  isProduction: false,
+  isProduction: process.env.MIDTRANS_IS_PRODUCTION,
   serverKey: process.env.MIDTRANS_SERVER_KEY,
   clientKey: process.env.MIDTRANS_CLIENT_KEY,
 });
 
 //SNAP API Instance
 let snap = new midtransClient.Snap({
-  isProduction: false,
+  isProduction: process.env.MIDTRANS_IS_PRODUCTION,
   serverKey: process.env.MIDTRANS_SERVER_KEY,
   clientKey: process.env.MIDTRANS_CLIENT_KEY,
 });
