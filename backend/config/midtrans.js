8c9f12ed17142fddb53d5d7b9d60d24a5cfd1f62
Web Developer BAS <webdeveloper.bas@gmail.com>
2026-05-25 17:08:42 +0700

    Exporting module constants and functions

--- backend/config/midtrans.js
+++ backend/config/midtrans.js
@@ -1,15 +1,17 @@
 const midtransClient = require("midtrans-client");
 
 //core API instance
-let coreApi = new midtransClient.CoreApi({
+const coreApi = new midtransClient.CoreApi({
   isProduction: process.env.MIDTRANS_IS_PRODUCTION,
   serverKey: process.env.MIDTRANS_SERVER_KEY,
   clientKey: process.env.MIDTRANS_CLIENT_KEY,
 });
 
 //SNAP API Instance
-let snap = new midtransClient.Snap({
+const snap = new midtransClient.Snap({
   isProduction: process.env.MIDTRANS_IS_PRODUCTION,
   serverKey: process.env.MIDTRANS_SERVER_KEY,
   clientKey: process.env.MIDTRANS_CLIENT_KEY,
 });
+
+module.exports = { coreApi, snap };
