f9625ff3875c747422cfbae637df5753bcf34298
Web Developer BAS <webdeveloper.bas@gmail.com>
2026-06-04 17:49:43 +0700

    Create the Completed page and create the StatusCard component for checking the payment status on the Complete Card

--- frontend/src/Pages/Completed/Completed.jsx
+++ frontend/src/Pages/Completed/Completed.jsx
@@ -1,8 +1,37 @@
+import { useSearchParams } from "react-router";
+import StatusCard from "./StatusCard/StatusCard";
+
 const CompletedPage = () => {
+  const [searchParams] = useSearchParams();
+
+  const orderId = searchParams.get("order_id");
+
+  const transactionStatus = searchParams.get("transaction_status");
+
+  const getStatusType = () => {
+    switch (transactionStatus) {
+      case "settlement":
+      case "capture":
+        return "success";
+
+      case "pending":
+        return "pending";
+
+      case "deny":
+      case "cancel":
+      case "expire":
+      case "failure":
+        return "failed";
+
+      default:
+        return "pending";
+    }
+  };
+
   return (
-    <>
-      <div>This is the Payment Page</div>
-    </>
+    <div className="completed-page">
+      <StatusCard status={getStatusType()} orderId={orderId} />
+    </div>
   );
 };
 
