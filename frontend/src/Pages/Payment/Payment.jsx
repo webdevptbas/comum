84abe9de66aeb8ed0b2508b4fa2b6675c6e0e105
Web Developer BAS <webdeveloper.bas@gmail.com>
2026-06-04 17:47:27 +0700

    Create payment page and include snap.js from Midtrans

--- frontend/src/Pages/Payment/Payment.jsx
+++ frontend/src/Pages/Payment/Payment.jsx
@@ -1,23 +1,79 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router";
+import { useEffect } from "react";
+import { useParams } from "react-router";
+import { useGetOrderDetailsQuery } from "../../Slices/ordersApiSlice";
+import { Card } from "antd";
+import PaymentSummary from "../Checkout/PaymentSummary";
+import "./Payment.css";
 
 const PaymentPage = () => {
-  const [paymentMethod, setPaymentMethod] = useState("");
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const { shippingAddress } = useSelector((state) => state.cart);
+  const { id: orderId } = useParams();
+  const {
+    data: order,
+    refetch,
+    isLoading,
+    error,
+  } = useGetOrderDetailsQuery(orderId);
+  const snapToken = order?.snapToken;
 
   useEffect(() => {
-    if (!shippingAddress) {
-      navigate("/shipping");
-    }
-  }, [shippingAddress, navigate]);
+    // load Midtrans Snap script
+    const script = document.createElement("script");
+    script.src = process.env.REACT_APP_MIDTRANS_URL;
+    script.setAttribute(
+      "data-client-key",
+      process.env.REACT_APP_MIDTRANS_CLIENT_KEY,
+    );
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  const handlePayment = () => {
+    if (!snapToken) return;
+
+    window.snap.pay(snapToken, {
+      onSuccess: function (result) {
+        console.log("SUCCESS:", result);
+      },
+      onPending: function (result) {
+        console.log("PENDING:", result);
+      },
+      onError: function (result) {
+        console.log("ERROR:", result);
+      },
+      onClose: function () {
+        console.log("User closed popup");
+      },
+    });
+  };
 
   return (
     <>
-      <div>This is the Payment Page</div>
+      <div className="payment-container">
+        <div className="payment-left">
+          <Card className="payment-info-card">
+            <h5 className="heading5 payment-title">Secure Payment</h5>
+            <p className="text-l-regular payment-subtitle">
+              All payments are securely processed by Midtrans. You will be able
+              to choose your preferred payment method in the next step.
+            </p>
+          </Card>
+        </div>
+
+        <div className="payment-right">
+          <PaymentSummary
+            onClick={handlePayment}
+            error={error}
+            buttonText={"Pay Now"}
+            isLoading={isLoading}
+            paymentData={order}
+          />
+        </div>
+      </div>
     </>
   );
 };
