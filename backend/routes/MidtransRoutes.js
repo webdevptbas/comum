dccc50dac6e800a1d72c3124f7a5d632c18458ca
Web Developer BAS <webdeveloper.bas@gmail.com>
2026-06-04 17:43:39 +0700

    Delete unused midtrans controller and routes

--- backend/routes/MidtransRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createPayment } = require("../controllers/MidtransController");
-
-//PRIVATE USER ROUTE
-router.post("/", createPayment);
-
-module.exports = router;
