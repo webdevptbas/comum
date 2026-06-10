b19ce0c978381ede49e985a11942faef4c30167b
Web Developer BAS <webdeveloper.bas@gmail.com>
2026-06-10 18:10:08 +0700

    Create new API fetch endpoint

--- admin/src/Util/apiService.js
+++ admin/src/Util/apiService.js
@@ -86,6 +86,19 @@ export const importProductsFromCsv = async (formData) => {
   }
 };
 
+//----------------------
+//-----ORDERS API-------
+//----------------------
+export const fetchAllOrders = async () => {
+  try {
+    const response = await api.get("/orders");
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    throw error;
+  }
+};
+
 //------------------------
 //-----EVENT LIST API-----
 //------------------------
