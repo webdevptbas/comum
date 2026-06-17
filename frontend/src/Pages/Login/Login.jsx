d7048eccb487271305d9c5caa313ff36781794f7
Web Developer BAS <webdeveloper.bas@gmail.com>
2026-06-17 11:00:55 +0700

    Add duration on message

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -24,7 +24,7 @@ const LoginPage = () => {
     const expiredMessage = sessionStorage.getItem("sessionExpired");
 
     if (expiredMessage) {
-      message.warning(expiredMessage, 5);
+      message.warning(expiredMessage, 7);
 
       sessionStorage.removeItem("sessionExpired");
     }
