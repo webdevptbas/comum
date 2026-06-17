3d897c12960da83125b3068660bfb7abea4422be
Web Developer BAS <webdeveloper.bas@gmail.com>
2026-06-17 11:13:23 +0700

    Create Change Password page

--- frontend/src/Pages/ChangePassword/ChangePassword.jsx
+++ frontend/src/Pages/ChangePassword/ChangePassword.jsx
@@ -1,5 +1,88 @@
-const ChangePasswordPage = () => {
-  return <h2>Change Password Page</h2>;
+import { Card, Form, Input, Button, message } from "antd";
+import { useChangePasswordMutation } from "../../Slices/usersApiSlice";
+
+const ChangePassword = () => {
+  const [form] = Form.useForm();
+  const [changePassword, { isLoading }] = useChangePasswordMutation();
+
+  const onFinish = async (values) => {
+    try {
+      await changePassword({
+        currentPassword: values.currentPassword,
+        newPassword: values.newPassword,
+      }).unwrap();
+
+      message.success("Password changed successfully");
+
+      form.resetFields();
+    } catch (err) {
+      message.error(err?.data?.message || "Failed to change password");
+    }
+  };
+
+  return (
+    <>
+      <Form form={form} layout="vertical" onFinish={onFinish}>
+        <Form.Item
+          label="Current Password"
+          s
+          name="currentPassword"
+          rules={[
+            {
+              required: true,
+              message: "Please enter your current password",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          label="New Password"
+          name="newPassword"
+          rules={[
+            {
+              required: true,
+              message: "Please enter a new password",
+            },
+            {
+              min: 8,
+              message: "Password must be at least 8 characters",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          label="Confirm New Password"
+          name="confirmPassword"
+          dependencies={["newPassword"]}
+          rules={[
+            {
+              required: true,
+              message: "Please confirm your password",
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("newPassword") === value) {
+                  return Promise.resolve();
+                }
+
+                return Promise.reject(new Error("Passwords do not match"));
+              },
+            }),
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Button type="primary" htmlType="submit" loading={isLoading}>
+          Change Password
+        </Button>
+      </Form>
+    </>
+  );
 };
 
-export default ChangePasswordPage;
+export default ChangePassword;
