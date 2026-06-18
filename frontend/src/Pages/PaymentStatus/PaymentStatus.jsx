b87581d3352ed4f8008c1fec30adba197484a2ba
Web Developer BAS <webdeveloper.bas@gmail.com>
2026-06-18 17:48:30 +0700

    Remove sync payment from automatically refresh the Payment Status

--- frontend/src/Pages/PaymentStatus/PaymentStatus.jsx
+++ frontend/src/Pages/PaymentStatus/PaymentStatus.jsx
@@ -1,30 +1,14 @@
 import { useSearchParams } from "react-router";
 import StatusCard from "./StatusCard/StatusCard";
 import { useSyncPaymentStatusMutation } from "../../Slices/ordersApiSlice";
-import { useEffect, useState } from "react";
-import { message, Skeleton } from "antd";
+import { useState } from "react";
+import { Skeleton } from "antd";
 
 const PaymentStatusPage = () => {
   const [searchParams] = useSearchParams();
   const [order, setOrder] = useState(null);
   const orderId = searchParams.get("order_id");
   const [syncPaymentStatus, { isLoading }] = useSyncPaymentStatusMutation();
-
-  useEffect(() => {
-    const syncPayment = async () => {
-      try {
-        if (!orderId) return;
-
-        const updatedOrder = await syncPaymentStatus(orderId).unwrap();
-
-        setOrder(updatedOrder);
-      } catch (err) {
-        message.error(err?.data?.message || "Failed to sync payment status");
-      }
-    };
-
-    syncPayment();
-  }, [orderId, syncPaymentStatus]);
 
   const getStatusType = () => {
     const transactionStatus = order?.paymentResult?.status;
